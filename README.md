f2b05d5a5574625d219c3a5e05dd4c228c492632
DrAtomic <leo.ward.garcia@gmail.com>
2021-06-16 23:40:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# company-gambit
+
+![example](Images/screenshot.png)
+
+a backend for company gambit
+
+## Description
+This is a very simple company backend for gambit, for emacs
+
+It finds all the procedures in the gambit documentation and provides them to [company](https://github.com/company-mode/company-mode).
+
+### why?
+I'm doing it to learn a few things, company backends, regular expressions, lisp in general.
+
+In addition gambit comes with a pretty nice [gambit-mode](https://github.com/gambit/gambit/blob/master/misc/gambit.el), the only thing missing was autocomplete, hence this project. 
+
+"why don't you just use [geiser](https://gitlab.com/emacs-geiser/gambit)?"
+
+you should definitely just use geiser
+
+## Installation
+```
+cd ~/.emacs.d
+git clone https://github.com/DrAtomic/company-gambit.git
+```
+
+go into your emacs config file and add
+
+```
+(use-package company-gambit
+  :ensure nil
+  :load-path "~/.emacs.d/company-gambit/"
+  :config
+  (defun my-scheme-mode-hook ()
+    (add-to-list 'company-backends 'company-gambit--backend))
+
+  (add-hook 'scheme-mode-hook 'my-scheme-mode-hook))
+
+```
+
+now start up a .scm file and you're good to go!
---
+++ company-gambit.el
@@ -0,0 +1,41 @@
+(require 'company)
+
+;;;###autoload
+(defun company-gambit--backend (command &optional arg &rest ignored)
+  (case command
+    (prefix (and (eq major-mode 'scheme-mode)
+                 (company-grab-symbol)))
+    
+    (candidates (all-completions
+                 arg
+                 (if (and (boundp 'procedure-hash)
+                          procedure-hash)
+                     procedure-hash
+		   
+		   (with-temp-buffer
+		     (insert-file-contents "/usr/share/doc/gambit-c/gambit.txt")
+		     (goto-char (point-min))
+		     (let ((test-list '()))
+		       (while (re-search-forward "-- procedure.*" (point-max) t)
+			 (let ((procedure-name-with-args (cadr (split-string (match-string 0) "-- procedure: "))))
+			   (push (car (split-string procedure-name-with-args)) test-list)))
+		       (setq procedure-hash test-list))))))))
+
+
+					; TODO: make the arguments list show up, also maybe use a hashtable instead of a list
+					; TODO: make it also match special forms
+
+;; (defun gambit-hash ()
+;;   "makes hash of procedure names as well as arguments"
+;;   (with-temp-buffer
+;;     (insert-file-contents "/usr/share/doc/gambit-c/gambit.txt")
+;;     (goto-char (point-min))
+;;     (let ((hash (make-hash-table :test #'equal)))
+;;       (while (re-search-forward "-- procedure.*\|-- special form:.*" (point-max) t)
+;; 	(let ((procedure-name-with-args (cadr (split-string (match-string 0) "-- procedure: "))))
+;; 	  (puthash (car (split-string procedure-name-with-args))
+;; 		   (cdr (split-string procedure-name-with-args))
+;; 		   hash)))
+;;       (setq procedure-hash hash))))
+
+(provide 'company-gambit)
